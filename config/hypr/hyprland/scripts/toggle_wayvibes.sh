e8c720564104ec995fe27fb1021dc3b8a4771b8a
Hieu Nguyen <hieuteo03@gmail.com>
2025-12-10 16:28:43 +0700

    Refactor color configurations and update GTK themes for improved aesthetics and consistency across multiple files

---
+++ config/hypr/hyprland/scripts/toggle_wayvibes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if wayvibes is running
+if pgrep -x wayvibes > /dev/null; then
+    # Kill wayvibes
+    pkill -x wayvibes
+    notify-send -u low -t 2000 "Wayvibes" "Keyboard sounds disabled"
+else
+    # Start wayvibes
+    wayvibes /home/ultimatebrok/Downloads/eg-oreo --background
+    notify-send -u low -t 2000 "Wayvibes" "Keyboard sounds enabled"
+fi
